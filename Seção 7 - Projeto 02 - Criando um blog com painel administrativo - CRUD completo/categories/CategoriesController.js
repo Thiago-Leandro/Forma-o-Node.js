b98be1f95c843eed10887c65e967d8435923cfa5
Thiago Leandro <tiago14789@live.com>
2024-02-12 17:19:58 -0300

    82. Tabelas do bootstrap

--- Seção 7 - Projeto 02 - Criando um blog com painel administrativo - CRUD completo/categories/CategoriesController.js
+++ Seção 7 - Projeto 02 - Criando um blog com painel administrativo - CRUD completo/categories/CategoriesController.js
@@ -21,7 +21,10 @@ router.post("/categories/save", (req, res) => {
     }else{
         res.redirect("/admin/categories/new");
     }
-    //res.render("categories/save");
+});
+
+router.get("/admin/categories", (req, res) => {
+    res.render("admin/categories/index");
 });
 
 module.exports = router;
