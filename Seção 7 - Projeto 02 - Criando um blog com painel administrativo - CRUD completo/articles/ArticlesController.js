9ebbfd23daa0d14d69c4533e4ee46438b3f79abc
Thiago Leandro <tiago14789@live.com>
2024-02-18 18:06:23 -0300

    94. Salvando artigos

--- Seção 7 - Projeto 02 - Criando um blog com painel administrativo - CRUD completo/articles/ArticlesController.js
+++ Seção 7 - Projeto 02 - Criando um blog com painel administrativo - CRUD completo/articles/ArticlesController.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const article = require("./Article");
+const slugify = require("slugify");
+const Article = require("./Article");
 
 router.get("/articles", (req, res) => {
     res.send("ROTA DE ARTIGO")
@@ -10,6 +13,20 @@ router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new",{categories: categories})
     })
+});
+
+router.post("./Articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var Category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    })
+
 });
 
 module.exports = router;
